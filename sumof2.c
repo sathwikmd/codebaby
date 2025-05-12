e1f386b8de89e836b4dae78e2a0546c9be8e722e
sathwikmd <sathwikmd2006@gmail.com>
2025-05-12 23:09:45 +0530

    sumof2.c

---
+++ sumof2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,sum;
+    printf("Enter the two numbers to calculate the sum:\n");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("The sum of two numbers is:%d",sum);
+    return 0;
+}
