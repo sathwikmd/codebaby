c5b470a81d234eb487cf1211fab3774d48e1f6d7
sathwikmd <sathwikmd2006@gmail.com>
2025-05-12 22:57:26 +0530

    cubeofnumber.c

---
+++ cubeofnumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int n,cube;
+   printf("enter the number to calculate the cube:");
+   scanf("%d",&n);
+   cube=(n*n*n);
+   printf("The cube of the entered number is:%d\n",cube);
+return 0;
+}
