e4b1e3c13e41b74810581c0de1851c73164c08c4
Sathwik MD <sathwikmd2006@gmail.com>
2025-05-13 14:17:53 +0530

    sizemanual.c

---
+++ sizemanual.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i;
+    float f;
+    char ch;
+    double d;
+    printf("The size of int:%d\n",i);
+    printf("the size of float:%f\n",f);
+    printf("The size of character:%d\n",ch);
+    printf("The size of double:%lf\n",d);
+}
