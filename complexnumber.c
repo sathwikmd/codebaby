e6737917be4285cc7a20bb2ccf5a2e736765e185
Sathwik MD <sathwikmd2006@gmail.com>
2025-05-13 14:33:39 +0530

    complexnumber.c

---
+++ complexnumber.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float c1;
+    float i=-1,a,b;
+    printf("Enter the value of a and b:\n");
+    scanf("%f %f",&a,&b);
+    c1=a+i*b;
+    printf("The sum of complex number is:%f\n",c1);
+    
+}
