9e28cb54b177d9661a218c059cfdb664b4aa71d6
Sathwik MD <sathwikmd2006@gmail.com>
2025-05-13 12:11:52 +0530

    asciivalueofthecharacter.c

---
+++ asciivalueofthecharacter.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    char ch;
+    printf("Enter the character to calculate its ASCII value:\n");
+    scanf("%c",&ch);
+    printf("The ASCII value of entered character is:%d",ch);
+}
