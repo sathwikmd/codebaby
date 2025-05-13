35f356060539ddd9017249f8a43243883e8ebff8
Sathwik MD <sathwikmd2006@gmail.com>
2025-05-13 21:56:54 +0530

    assif.c
    assignment operator as equivalent operator this program is physch

---
+++ assif.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int x=2;
+   if(x=1)
+   {
+       printf("x is equal to one\n");
+   }
+   else
+   {
+       printf("x is not equal to one\n");
+   }
+    return 0;
+}
