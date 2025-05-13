1ff20b8dd6b1f29d6947fd6dda7964e1f59ce770
Sathwik MD <sathwikmd2006@gmail.com>
2025-05-13 20:57:23 +0530

    ] weekdayusingswitch.c

---
+++ weekdayusingswitch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int day;
+    printf("enter the day(1-7):\n");
+    scanf("%d",&day);
+    switch(day)
+    {
+        case 1:
+        printf("monday");
+        break;
+        case 2:
+        printf("tuesday");
+        break;
+        case 3:
+        printf("wednesday");
+        break;
+        case 4:
+        printf("thursday");
+        break;
+        case 5:
+        printf("friday");
+        break;
+        case 6:
+        printf("saturday");
+        break;
+        case 7:
+        printf("sunday");
+        break;
+        default:
+        printf("not a valid number\n");
+    }
+    return 0;
+}
