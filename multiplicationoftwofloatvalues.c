9e541e40e7bd664869d29a6b8a86d66705f68fa8
Sathwik MD <sathwikmd2006@gmail.com>
2025-05-13 12:02:41 +0530

    multiplicationoftwofloatvalues.c

---
+++ multiplicationoftwofloatvalues.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float op1,op2,sum;
+    printf("Enter two numbers to calculate the sum:\n");
+    scanf("%f %f",&op1,&op2);
+    sum=op1*op2;
+    printf("The sum of two numbers is:%f",sum);
+}
