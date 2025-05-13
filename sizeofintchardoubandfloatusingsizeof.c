61eb9c19b7e25d4cf384100e62127796fcea58f8
Sathwik MD <sathwikmd2006@gmail.com>
2025-05-13 14:23:08 +0530

    sizeofintchardoubandfloatusingsizeof.c
    using size of operator

---
+++ sizeofintchardoubandfloatusingsizeof.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i;
+    float f;
+    char ch;
+    double d;
+    printf("The size of int:%u\n",sizeof(i));
+    printf("the size of float:%u\n",sizeof(f));
+    printf("The size of character:%u\n",sizeof(ch));
+    printf("The size of double:%u\n",sizeof(d));
+}
